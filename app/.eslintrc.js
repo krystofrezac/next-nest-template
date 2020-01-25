df12a654d1e04bb63ea826740943825fc671bb1e
Kryštof Řezáč <krystofrezac@gmail.com>
2020-01-25 15:58:22 +0100

    Add eslint

---
+++ app/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'plugin:react/recommended',
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    "prettier",
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react/jsx-filename-extension': 'off',
+  },
+};
