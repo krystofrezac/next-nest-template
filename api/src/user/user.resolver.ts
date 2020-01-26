a1a7fbbbb9121ef474f93f97b95959e01845b696
Kryštof Řezáč <krystofrezac@gmail.com>
2020-01-26 16:08:51 +0100

    Add api to docker

--- api/src/user/user.resolver.ts
+++ api/src/user/user.resolver.ts
@@ -1,9 +1,11 @@
-import { Query } from '@nestjs/graphql';
+import { Args, Query } from '@nestjs/graphql';
+import { UnauthorizedException } from '@nestjs/common';
 import User from './user.entity';
 
 class UserResolver {
   @Query(() => User)
-  async userGet() {
+  async userGet(@Args({ name: 'password', type: () => String }) password: string) {
+    if (password !== 'heslo') throw new UnauthorizedException();
     const u = new User();
     u.id = 123;
     return u;
