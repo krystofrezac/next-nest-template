2777d69ef7507e096f20165d91b7444acd4a6578
Kryštof Řezáč <krystofrezac@gmail.com>
2020-01-27 10:33:22 +0100

    Fix connecting api to db

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import UserModule from './user/user.module';
 
@@ -8,6 +9,16 @@ import UserModule from './user/user.module';
     UserModule,
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'db',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'template',
+      entities: [],
+      synchronize: true,
     }),
   ],
 })
