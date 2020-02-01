98efdf92da4aecff3e1328aa99df497f5ce239d3
Kryštof Řezáč <krystofrezac@gmail.com>
2020-02-01 11:24:39 +0100

    Material ui work

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -3,9 +3,21 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import App from 'next/app';
 import withRedux from 'next-redux-wrapper';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Head from 'next/head';
 import makeStore from '../redux';
+import theme from '../src/theme';
 
 class MyApp extends App<{ store: any }> {
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }
+
   static async getInitialProps({ Component, ctx }) {
     ctx.store.dispatch({ type: 'FOO', payload: 'foo' });
 
@@ -17,9 +29,19 @@ class MyApp extends App<{ store: any }> {
   render() {
     const { Component, pageProps, store } = this.props;
     return (
-      <Provider store={store}>
-        <Component {...pageProps} />
-      </Provider>
+      <>
+        <Head>
+          <title>Template</title>
+          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        </Head>
+        <Provider store={store}>
+          <ThemeProvider theme={theme}>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </Provider>
+      </>
     );
   }
 }
