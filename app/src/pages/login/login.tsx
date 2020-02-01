adf035fa2c5398579ec2a6a9520a47514b77c51e
Kryštof Řezáč <krystofrezac@gmail.com>
2020-02-01 11:54:17 +0100

    add react hook form

--- app/src/pages/login/login.tsx
+++ app/src/pages/login/login.tsx
@@ -5,13 +5,11 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import { useForm } from 'react-hook-form';
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -35,7 +33,11 @@ const useStyles = makeStyles(theme => ({
 
 const Login = () => {
   const classes = useStyles();
+  const { handleSubmit, register, errors } = useForm();
 
+  const onSubmit = values => {
+    console.log(values);
+  };
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -46,11 +48,12 @@ const Login = () => {
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
-        <form className={classes.form} noValidate>
+        <form onSubmit={handleSubmit(onSubmit)} className={classes.form} noValidate>
           <TextField
+            inputRef={register({ required: true })}
+            error={errors.email !== undefined}
             variant="outlined"
             margin="normal"
-            required
             fullWidth
             id="email"
             label="Email Address"
@@ -59,9 +62,10 @@ const Login = () => {
             autoFocus
           />
           <TextField
+            inputRef={register({ required: true })}
+            error={errors.password !== undefined}
             variant="outlined"
             margin="normal"
-            required
             fullWidth
             name="password"
             label="Password"
